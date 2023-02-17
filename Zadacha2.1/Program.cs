3ff6b8a0d3bd4725f8c4d662443e6e5eec797d99
AleksDerivolkov <124775973+AleksDerivolkov@users.noreply.github.com>
2023-02-17 13:05:54 +0200

    Add files via upload

---
+++ Zadacha2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zadacha2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] row1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+            string[] row2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < row2.Length; i++)
+            {
+                if (row1.Contains(row2[i]))
+                {
+                    Console.Write(row2[i]+" ");
+                }
+                
+            }
+
+        }
+    }
+}
